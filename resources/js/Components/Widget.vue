2f69aab1fed69a712175dcd110dd01953bd7c0da
sibber11 <sibber11@gmail.com>
2023-09-11 21:19:06 +0600

    widget component

---
+++ resources/js/Components/Widget.vue
@@ -0,0 +1,25 @@
+<script setup>
+import BaseIcon from '@/Components/BaseIcon.vue';
+const props = defineProps({
+    title: String,
+    value: String,
+    icon: String,
+});
+</script>
+<template>
+    <div class= "p-2 rounded shadow bg-gradient-to-r from-orange-400 to-pink-500 overflow-clip relative text-white">
+        <div class="w-20 h-20 rounded-full bg-white opacity-30 absolute -right-8 -top-2"></div>
+        <div class="w-24 h-24 rounded-full bg-white opacity-30 absolute right-0 -bottom-8"></div>
+        <div class="flex justify-between">
+            <div class="px-2">
+                <div class="uppercase text-sm tracking-wider font-semibold">{{ title }}</div>
+                <div class="text-xl font-bold">{{ value }}</div>
+            </div>
+            <BaseIcon :path="icon" class="text-white w-12 h-12" size="40" v-if="icon"/>
+        </div>
+        <div class="flex px-2 pt-4">
+            <div class="text-sm">Last 30 days</div>
+            <div class="text-sm">+ 5.2%</div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Components/Widgets.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    title: String,
+});
+</script>
+<template>
+    <div class="py-4">
+        <div class="py-6 text-gray-900 font-extrabold uppercase">
+            {{ title }}
+        </div>
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
+            <slot />
+        </div>
+    </div>
+</template>
