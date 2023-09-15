2fa3bb8d0b49d33ad6e3139573566708741853a2
sibber11 <sibber11@gmail.com>
2023-09-15 20:04:01 +0600

    link added to widget

--- resources/js/Components/Widget.vue
+++ resources/js/Components/Widget.vue
@@ -1,13 +1,16 @@
 <script setup>
 import BaseIcon from '@/Components/BaseIcon.vue';
+import { Link } from '@inertiajs/vue3';
 const props = defineProps({
     title: String,
     value: Number,
     icon: String,
+    href: String
 });
 </script>
 <template>
-    <div class= "p-2 rounded shadow bg-gradient-to-r from-orange-400 to-pink-500 overflow-clip relative text-white">
+    <Link :href="href">
+    <div class="p-2 rounded shadow bg-gradient-to-r from-orange-400 to-pink-500 overflow-clip relative text-white">
         <div class="w-20 h-20 rounded-full bg-white opacity-30 absolute -right-8 -top-2"></div>
         <div class="w-24 h-24 rounded-full bg-white opacity-30 absolute right-0 -bottom-8"></div>
         <div class="flex justify-between">
@@ -15,11 +18,12 @@ const props = defineProps({
                 <div class="uppercase text-sm tracking-wider font-semibold">{{ title }}</div>
                 <div class="text-xl font-bold">{{ value }}</div>
             </div>
-            <BaseIcon :path="icon" class="text-white w-12 h-12" size="40" v-if="icon"/>
+            <BaseIcon :path="icon" class="text-white w-12 h-12" size="40" v-if="icon" />
         </div>
         <div class="flex px-2 pt-4">
             <div class="text-sm">Last 30 days</div>
             <div class="text-sm">+ 5.2%</div>
         </div>
     </div>
+    </Link>
 </template>
