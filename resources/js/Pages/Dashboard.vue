d180bce3fd310872c249e00e8398103258b88fbf
sibber11 <sibber11@gmail.com>
2023-09-15 18:46:19 +0600

    many things fixed

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,6 +15,7 @@ import {
     Legend
 } from 'chart.js';
 import { Line } from 'vue-chartjs';
+import Pagination from './Admin/Components/Pagination.vue';
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -38,7 +39,7 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-bold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
         <!-- quick links -->
@@ -46,7 +47,7 @@ defineProps({
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-12">
-                <div class="p-6 text-gray-900">Hello <span>{{ $page.props.auth.user.name }}</span>. Welcome to
+                <div class="p-6 text-gray-900">Hello <span class="font-semibold">{{ $page.props.auth.user.name }}</span>. Welcome to
                     <span>Library Management.</span>
                 </div>
             </div>
@@ -54,9 +55,9 @@ defineProps({
                 Quick Links
             </h3>
             <div class="flex gap-4 flex-wrap">
-                <Link class="bg-orange-800" as="button" :href="route('check-outs.create')">New Checkout</Link>
-                <Link class="bg-orange-800" as="button" :href="route('books.create')">New Book</Link>
-                <Link class="bg-orange-800" as="button" :href="route('members.create')">New Member</Link>
+                <Link class="bg-orange-500" as="button" :href="route('check-outs.create')">New Checkout</Link>
+                <Link class="bg-orange-500" as="button" :href="route('books.create')">New Book</Link>
+                <Link class="bg-orange-500" as="button" :href="route('members.create')">New Member</Link>
 
             </div>
             <Widgets title="Inventory Statistics">
@@ -74,7 +75,7 @@ defineProps({
                 <h3 class="py-6 text-gray-900 font-extrabold uppercase">
                     Usage Reports
                 </h3>
-                <Line :data="lineChart" />
+                <Line :data="lineChart" :options="{scales:{y:{min: 0,suggestedMax:10,ticks:{stepSize:1}}}}"/>
             </div>
 
             <div class="overflow-hidden shadow-sm sm:rounded-lg pb-4">
@@ -92,14 +93,18 @@ defineProps({
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="checkout in overdueBooks">
+                            <tr v-for="checkout in overdueBooks.data">
                                 <td>{{ checkout.id }}</td>
                                 <td>{{ checkout.book.title }}</td>
                                 <td>{{ checkout.member.user.name }}</td>
                                 <td>{{ checkout.check_out_date }}</td>
                             </tr>
+                            <tr v-if="overdueBooks.data.length == 0">
+                                <td colspan="4" class="font-semibold text-center text-lg">No Overdue Books.</td>
+                            </tr>
                         </tbody>
                     </table>
+                    <Pagination :links="overdueBooks.links" />
                 </div>
             </div>
         </div>
